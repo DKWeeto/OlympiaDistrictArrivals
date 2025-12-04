94a62fdac053066561680d3c3c65991af8dde357
DKWeeto <danaweetman@gmail.com>
2025-12-04 19:32:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Setup
+Run `pip install -r requirements.txt`
+Create a `.env` file with your `API_KEY` variable
+
+## Run
+Run `python3 main.py`
---
+++ main.py
@@ -0,0 +1,36 @@
+from requests import get
+from dateutil import parser
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+base_url = "https://api.tfl.gov.uk"
+
+def get_arrivals(stop_id):
+    url = f"{base_url}/StopPoint/{stop_id}/Arrivals?app_key={api_key}"
+    response = get(url, timeout=10)
+
+
+    try:
+        data = response.json()
+        return data
+    except Exception as e:
+        print("JSON ERROR:", e)
+        return None
+
+if __name__ == '__main__':
+    arrivals = get_arrivals("940GZZLUKOY") # Stop id here
+    for a in arrivals:
+        exp_arr = parser.isoparse(a['expectedArrival'])
+        
+        print('Line: ', a['lineName'])
+        print('Platform: ', a['platformName'])
+        if 'destinationName' in a:
+            print('Destination: ', a['destinationName'])
+        else:
+            print('Towards: ', a['towards'])
+        print('Expected Arrival: ', exp_arr.strftime("%H:%M:%S"))
+        print('Current Location: ', a['currentLocation'])
+        print('\n')
+
